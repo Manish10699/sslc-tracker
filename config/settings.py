630eb0565e8463b6737027fb5ad9907e89b85c05
manish <manishnk1431@gmail.com>
2026-08-21 12:07:20 +0530

    Add ALLOWED_HOSTS for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "sslc-tracker.onrender.com",'localhost', '127.0.0.1'
+]
 
 
 # Application definition
